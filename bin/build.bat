140115cbce0335893e644d7f5aa9f4180d425710
Serge Ratke <serge.ratke@gmail.com>
2019-10-25 19:35:54 +0200

    travis

--- bin/build.bat
+++ bin/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set compiler_flags=-Od -MTd -nologo -DWEBC_LOAD_MODULES=1 -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4201 -wd4100 -wd4101 -wd4189 -wd4505 -wd4127 -wd4702 -FC -Z7 -I%PROJECT_PATH%/src -I%PROJECT_PATH%/lib -I%CPP_LIB_PATH%
+set compiler_flags=-Od -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4201 -wd4100 -wd4101 -wd4189 -wd4505 -wd4127 -wd4702 -FC -Z7 -I%PROJECT_PATH%/src
 set linker_flags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib Shlwapi.lib Ws2_32.lib
 
 IF NOT exist %BUILD_PATH% ( mkdir %BUILD_PATH% )
---
+++ bin/travis_build.bat
@@ -0,0 +1,42 @@
+echo off
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" amd64
+        echo "USING VISUAL STUDIO 15"
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+        echo "USING VISUAL STUDIO 14"
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" amd64
+        echo "USING VISUAL STUDIO 13"
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+        echo "USING VISUAL STUDIO 12"
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    echo "No compatible visual studio found! run vcvarsall.bat first!"
+)
+
+mkdir build
+pushd build
+
+set compiler_flags=-Od -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -wd4201 -wd4100 -wd4101 -wd4189 -wd4505 -wd4127 -wd4702 -FC -Z7 -I../src
+set linker_flags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib Shlwapi.lib Ws2_32.lib
+cl %compiler_flags% ..\src\main.cpp -Fetempl.exe /link %linker_flags%
+
+popd
